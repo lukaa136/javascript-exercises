c1d4bae8215431ace2af74fa078110cfb258810a
Luca <avilaluca61@gmail.com>
2025-01-30 16:53:23 -0300

    Finished pseudocode

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-};
+function searchValue(myArray, value) {
+  if (myArray.includes(value)) {
+    return true;
+  }
+  return false;
+  /* agregar que vaya fijandose indice por indice
+  quizasss:
+    for (let i = 0; i <= array.length - 1; i++) {
+    if (myArray[i] == value)} */
+}
 
+const removeFromArray = function (array, deletedElement) {};
+/* funcion  (array, elementoAEliminar) {
+si el valor del primer indice tiene el elemento a eliminar, borrarlo, sino pasar al siguiente
+si es asi, eliminar ese elemento
+devolver el nuevo array
+}*/
 // Do not edit below this line
 module.exports = removeFromArray;
