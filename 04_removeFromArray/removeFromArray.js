ee491371505ae4c67749462caad2fb72c31bdf4a
Luca <avilaluca61@gmail.com>
2025-01-31 15:37:42 -0300

    complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,19 +1,26 @@
-function searchValue(myArray, value) {
-  if (myArray.includes(value)) {
-    return true;
-  }
-  return false;
-  /* agregar que vaya fijandose indice por indice
-  quizasss:
-    for (let i = 0; i <= array.length - 1; i++) {
-    if (myArray[i] == value)} */
-}
+const removeFromArray = function (array, ...deletedElement) {
+  const newArray = [];
 
-const removeFromArray = function (array, deletedElement) {};
-/* funcion  (array, elementoAEliminar) {
-si el valor del primer indice tiene el elemento a eliminar, borrarlo, sino pasar al siguiente
-si es asi, eliminar ese elemento
-devolver el nuevo array
-}*/
+  array.forEach((element) => {
+    if (!deletedElement.includes(element)) {
+      newArray.push(element);
+    }
+  });
+
+  return newArray;
+};
+
+/* const removeFromArray = function (array, ...args) {
+  const newArray = [];
+
+  array.forEach((item) => {
+    if (!args.includes(item)) {
+      newArray.push(item);
+    }
+  });
+
+  return newArray;
+}; */
+
 // Do not edit below this line
 module.exports = removeFromArray;
