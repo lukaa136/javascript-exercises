b5a80234ec824a785df3aa7ac6f6d083dc46d104
Luca <avilaluca61@gmail.com>
2025-01-31 15:50:23 -0300

    complete 4/7 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
+  if (num1 < num2) {
+    largestNum = num2;
+    smallestNum = num1;
+  } else {
+    largestNum = num1;
+    smallestNum = num2;
+  }
+  let counter = 0;
+  for (let i = smallestNum; i <= largestNum; i++) {
+    counter += i;
+  }
+  return counter;
 };
 
 // Do not edit below this line
