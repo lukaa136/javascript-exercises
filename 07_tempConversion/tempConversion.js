50b5e6e689ced965c1d06bed992bf61a18872985
Luca <avilaluca61@gmail.com>
2025-02-01 13:08:12 -0300

    complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheitDegrees) {
+  let celciusDegrees = (fahrenheitDegrees - 32) * (5 / 9);
+  if (parseInt(celciusDegrees) == celciusDegrees) {
+    return celciusDegrees;
+  }
+  let rounded = celciusDegrees.toFixed(1);
+  return parseFloat(rounded);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celciusDegrees) {
+  let fahrenheitDegrees = celciusDegrees * 1.8 + 32;
+  if (parseInt(fahrenheitDegrees) == fahrenheitDegrees) {
+    return fahrenheitDegrees;
+  }
+  let rounded = fahrenheitDegrees.toFixed(1);
+  return parseFloat(rounded);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
